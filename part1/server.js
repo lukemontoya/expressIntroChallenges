38e69029557266f69d80f9de9408f14ada370937
Luke Montoya <plane.luke@gmail.com>
2018-05-11 08:54:09 -0700

    Part 2 is break after second name add

--- part1/server.js
+++ part1/server.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var path = require("path");
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
 });
+
+app.post("/create/:name", function(req, res){
+  var objName = {"id":1,"name":""+req.params.name+""}
+  res.json(objName);
+})
+
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname + "/index.html"));
+})
+
+app.get("/verify/:age", function(req, res){
+  // var objAge = {"age1": 30, "age2": 12};
+  if(req.params.age < 13){
+    res.sendStatus(403)
+  } else
+  res.sendStatus(200)
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,47 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8000;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+const fs = require("fs");
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
 });
+
+
+app.post("/create/:name/:age", (req, res) => {
+  let newUser = {
+    name: req.params.name,
+    age: req.params.age
+  }
+
+  let strCurrentArr = fs.readFileSync("./storage.json", "utf8");
+
+  let currentArr = JSON.parse(strCurrentArr);
+
+  currentArr.push(newUser);
+
+  fs.appendFileSync('./storage.json', JSON.stringify(currentArr));
+
+  res.send("for the win");
+});
+
+app.get("/", function(req, res){
+  let data = fs.readFileSync("./storage.json", "utf8");
+  res.send(data)
+})
+
+app.get("/:name", function(req, res){
+  let strUsersArr = fs.readFileSync("./storage.json", "utf8");
+  let usersArr = JSON.parse(strUsersArr);
+  for(let i =0; i<usersArr.length; i++){
+    if(usersArr[i].name == req.params.name){
+    res.json(usersArr[i]);
+    return;
+    }
+  }
+  res.sendStatus(400);
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
